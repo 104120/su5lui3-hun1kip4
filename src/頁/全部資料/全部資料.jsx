b0f1125a3a4f8d444c599cb37284a3f622804ffa
薛丞宏 <ihcaoe@gmail.com>
2016-11-24 10:56:04 +0800

    全部資料正常顯示出來矣

--- src/頁/全部資料/全部資料.jsx
+++ src/頁/全部資料/全部資料.jsx
@@ -3,15 +3,42 @@ import React from 'react';
 import superagent from 'superagent-bluebird-promise';
 import Debug from 'debug';
 import ReactDOM from 'react-dom';
+import 後端網址 from '../後端網址';
 
 var debug = Debug('kip4:全部資料');
 
 export default class 全部資料 extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {      
+    };
+  }
+
+  componentWillMount() {
+    superagent.get(後端網址.全部資料())
+      .then(({ body }) => (this.setState(body)))
+      .catch((err) => (debug(err)));
+  }
+
   render () {
+    let {資料}=this.state
+    if (資料==undefined)
+    {
     return (
       <div className='main container'>
-      XX
+       載入中…
+      </div>
+    ); 
+    }
+    let hian2si7=資料.map((tsit8pit4,i)=>(
+      <div key={i}>
+        {tsit8pit4.文章名} {tsit8pit4.作者} {tsit8pit4.聽拍的人}
+      </div>
+      ))
+    return (
+      <div className='main container'>
+        {hian2si7}
       </div>
     );
   }
---
+++ src/頁/後端網址.jsx
@@ -0,0 +1,9 @@
+export default class 後端網址  {
+  static 全部資料() {
+    return 'http://localhost:8000/';
+  }
+
+  static 報表網址() {
+    return 'http://localhost:8000/報表';
+  }
+}
