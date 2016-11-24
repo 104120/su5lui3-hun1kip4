6b9c713069272e57a897a3f7cd830dcccae6bb69
薛丞宏 <ihcaoe@gmail.com>
2016-11-24 15:47:31 +0800

    修後端網址

--- src/頁/後端網址.jsx
+++ src/頁/後端網址.jsx
@@ -8,7 +8,7 @@ export default class 後端網址  {
   static 看書面(pian1ho7) {
 
     // return 'http://localhost:8000/看書面/' + pian1ho7;
-    return 'http://xn--1xt27my50b.xn--kbr136hblivtn.xn--v0qr21b.xn--kpry57d/' + pian1ho7;
+    return 'http://xn--1xt27my50b.xn--kbr136hblivtn.xn--v0qr21b.xn--kpry57d/看書面/' + pian1ho7;
   }
 
   static 原始檔案(檔案) {
