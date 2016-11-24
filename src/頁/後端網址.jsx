ec2bf92340f6d135ecc91b1158b9ddb07c6bd599
薛丞宏 <ihcaoe@gmail.com>
2016-11-24 15:36:47 +0800

    改title佮後端網址

--- src/頁/後端網址.jsx
+++ src/頁/後端網址.jsx
@@ -1,13 +1,18 @@
 export default class 後端網址  {
   static 全部資料() {
-    return 'http://localhost:8000/';
+
+    // return 'http://localhost:8000/';
+    return 'http://xn--1xt27my50b.xn--kbr136hblivtn.xn--v0qr21b.xn--kpry57d/' ;
   }
 
   static 看書面(pian1ho7) {
-    return 'http://localhost:8000/看書面/' + pian1ho7;
+
+    // return 'http://localhost:8000/看書面/' + pian1ho7;
+    return 'http://xn--1xt27my50b.xn--kbr136hblivtn.xn--v0qr21b.xn--kpry57d/' + pian1ho7;
   }
 
   static 原始檔案(檔案) {
-    return 'http://localhost:8000' + 檔案;
+    // return 'http://localhost:8000' + 檔案;
+    return 'http://xn--1xt27my50b.xn--kbr136hblivtn.xn--v0qr21b.xn--kpry57d' + 檔案;
   }
 }
