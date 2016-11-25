62707365ac1a6916fa831a535e9a23cd4fbe9750
薛丞宏 <ihcaoe@gmail.com>
2016-11-25 18:58:40 +0800

    共網址模組化

--- src/頁/後端網址.jsx
+++ src/頁/後端網址.jsx
@@ -1,18 +1,22 @@
 export default class 後端網址  {
-  static 全部資料() {
+  static 主機() {
 
     // return 'http://localhost:8000/';
-    return 'http://xn--1xt27my50b.xn--kbr136hblivtn.xn--v0qr21b.xn--kpry57d/';
+    return 'http://xn--1xt27my50b.xn--kbr136hblivtn.xn--v0qr21b.xn--kpry57d';
+  }
+
+  static 全部資料() {
+    return this.主機();
   }
 
   static 看書面(pian1ho7) {
 
     // return 'http://localhost:8000/看書面/' + pian1ho7;
-    return 'http://xn--1xt27my50b.xn--kbr136hblivtn.xn--v0qr21b.xn--kpry57d/看書面/' + pian1ho7;
+    return this.主機() + '/看書面/' + pian1ho7;
   }
 
   static 原始檔案(檔案) {
     // return 'http://localhost:8000' + 檔案;
-    return 'http://xn--1xt27my50b.xn--kbr136hblivtn.xn--v0qr21b.xn--kpry57d' + 檔案;
+    return this.主機() + 檔案;
   }
 }
