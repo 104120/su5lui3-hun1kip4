4c95fa6b0491e45fb91bf38cc4f8b967d01022f2
薛丞宏 <ihcaoe@gmail.com>
2016-12-19 15:29:16 +0800

    修錯誤音標

--- src/元素/斷詞原則/斷詞原則.jsx
+++ src/元素/斷詞原則/斷詞原則.jsx
@@ -18,7 +18,7 @@ export default class 斷詞原則 extends React.Component {
                       <tr><td>有 法度</td><td>ū huat-tōo</td></tr>
                       <tr><td>掠人</td><td>lia̍h lâng</td></tr>
                       <tr><td>一个人</td><td>tsi̍t ê lâng</td></tr>
-                      <tr><td>一個月</td><td>tsi̍t kòo gue̍h</td></tr>
+                      <tr><td>一個月</td><td>tsi̍t kò gue̍h</td></tr>
                       <tr><td>遮爾</td><td>tsiah-nī</td></tr>
                     </tbody>
                   </table>
