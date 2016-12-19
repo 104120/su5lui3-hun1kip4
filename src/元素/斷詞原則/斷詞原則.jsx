1061f1f88cad393a3e37799ccb931e2f2a7d11d5
薛丞宏 <ihcaoe@gmail.com>
2016-12-19 12:58:27 +0800

    允言修斷詞原則

--- src/元素/斷詞原則/斷詞原則.jsx
+++ src/元素/斷詞原則/斷詞原則.jsx
@@ -11,15 +11,15 @@ export default class 斷詞原則 extends React.Component {
                       <th>臺羅</th>
                     </tr></thead>
                     <tbody>
-                      <tr><td>這个</td><td>tsit-ê</td></tr>
-                      <tr><td>無要緊</td><td>bô-iàu-kín</td></tr>
-                      <tr><td>有 要緊</td><td> iàu-kín</td></tr>
+                      <tr><td>這个</td><td>tsit ê</td></tr>
+                      <tr><td>無要緊</td><td>bô iàu-kín</td></tr>
+                      <tr><td>有 要緊</td><td>ū iàu-kín</td></tr>
                       <tr><td>無法度</td><td>bô-huat-tōo</td></tr>
                       <tr><td>有 法度</td><td>ū huat-tōo</td></tr>
-                      <tr><td>掠人</td><td>lia̍h-lâng</td></tr>
-                      <tr><td>一个人</td><td>tsi̍t-ê-lâng</td></tr>
-                      <tr><td>一個月</td><td>tsi̍t-kòo-gue̍h</td></tr>
-                      <tr><td>遮爾</td><td>tsiah-ní</td></tr>
+                      <tr><td>掠人</td><td>lia̍h lâng</td></tr>
+                      <tr><td>一个人</td><td>tsi̍t ê lâng</td></tr>
+                      <tr><td>一個月</td><td>tsi̍t kòo gue̍h</td></tr>
+                      <tr><td>遮爾</td><td>tsiah-nī</td></tr>
                     </tbody>
                   </table>
                 </div>
