07050abce3aabdfa7396b47a0ac7553c1312b4ba
薛丞宏 <ihcaoe@gmail.com>
2016-12-19 17:19:28 +0800

    共空白提掉

--- src/元素/斷詞原則/斷詞原則.jsx
+++ src/元素/斷詞原則/斷詞原則.jsx
@@ -13,9 +13,9 @@ export default class 斷詞原則 extends React.Component {
                     <tbody>
                       <tr><td>這个</td><td>tsit ê</td></tr>
                       <tr><td>無要緊</td><td>bô iàu-kín</td></tr>
-                      <tr><td>有 要緊</td><td>ū iàu-kín</td></tr>
+                      <tr><td>有要緊</td><td>ū iàu-kín</td></tr>
                       <tr><td>無法度</td><td>bô-huat-tōo</td></tr>
-                      <tr><td>有 法度</td><td>ū huat-tōo</td></tr>
+                      <tr><td>有法度</td><td>ū huat-tōo</td></tr>
                       <tr><td>掠人</td><td>lia̍h lâng</td></tr>
                       <tr><td>一个人</td><td>tsi̍t ê lâng</td></tr>
                       <tr><td>一個月</td><td>tsi̍t kò gue̍h</td></tr>
