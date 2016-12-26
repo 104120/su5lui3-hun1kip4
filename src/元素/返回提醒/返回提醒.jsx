fd7f78a7f71ea8b496c7229b895d608f7cf3fb6d
a8568730 <a8568730@hotmail.com>
2016-12-26 19:49:28 +0800

    修改跳出視窗樣式

--- src/元素/返回提醒/返回提醒.jsx
+++ src/元素/返回提醒/返回提醒.jsx
@@ -3,23 +3,36 @@ import Debug from 'debug';
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
 
+const customStyles = {
+  content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      padding: '0',
+      overflow: 'visible',
+    },
+};
+
+const headerStyle = {
+  marginBottom: '-1rem',
+};
+
+const contentStyle = {
+  marginTop: '0',
+};
+
+const closeStyle = {
+  boxShadow: 'none',
+  marginTop: '-0.5rem',
+  marginRight: '-0.7rem',
+};
+
 export default class 返回提醒 extends React.Component  {
   constructor(props) {
     super(props);
-  }
-
-  customStyles() {
-    return {
-      content: {
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)',
-        padding: '0',
-      },
-    };
   }
 
   render() {
@@ -28,19 +41,22 @@ export default class 返回提醒 extends React.Component  {
         <Modal
           isOpen={this.props.modalIsOpen}
           onRequestClose={this.props.closeModal.bind(this)}
-          style={this.customStyles()}
+          style={customStyles}
           contentLabel="返回提醒"
         >
-          <div className="ui clearing basic segment">
-            <h2 className="ui left floated header">提醒</h2>
-            <button className="ui basic icon right floated button" onClick={this.props.closeModal.bind(this)}>
+          <div className="ui clearing basic segment" style={headerStyle}>
+            <h3 className="ui left floated header">提醒</h3>
+            <button className="ui basic icon right floated button"
+              onClick={this.props.closeModal.bind(this)}
+              style={closeStyle}
+            >
               <i className="remove icon"></i>
             </button>
           </div>
-          <div className="ui basic segment">
+          <div className="ui clearing basic segment" style={contentStyle}>
             <p>文章被修改過，是否仍要離開？</p>
             <button className="ui basic button" onClick={this.props.closeModal.bind(this)}>繼續改</button>
-            <button className="ui green button" onClick={this.props.返回.bind(this)}>好，離開</button>
+            <button className="ui right floated green button" onClick={this.props.返回.bind(this)}>好，離開</button>
           </div>
         </Modal>
       </div>
