fc50b46c7946dab90a673c9d2a92f96866d35e65
a8568730 <a8568730@hotmail.com>
2016-12-28 12:52:40 +0800

    微調架構，加上網站名稱

--- src/網站/頁頭.jsx
+++ src/網站/頁頭.jsx
@@ -22,26 +22,17 @@ export default class 頁頭 extends React.Component {
   render () {
     return (
       <header className='app header'>
-        <div className='app bar container'>
-          <ul className='ui menu container inverted'>
-            <li className='item'>
-              <Link to='/'>首頁</Link>
-            </li>
-            <li className='item'>
-              <a href='#' onClick={this.open書寫規範.bind(this)}>書寫規範</a>
-            </li>
-            <li className='item'>
-              <Link to='/%E5%AD%97%E6%95%B8/'>字數</Link>
-            </li>
-            <li className='item'>
-              <a href='https://www.facebook.com/groups/1176719339085090/1182176585206032/?notif_t=group_activity&notif_id=1479125586448144'>FB社團</a>
-            </li>
-            <li className='item'>
-              <a href='https://github.com/sih4sing5hong5/su5lui3-hun1kip4'>網站程式碼</a>
-            </li>
-          </ul>
-        </div>
-
+          <div className='ui stackable menu container inverted'>
+            <div className="header item">
+              <i className="ui book icon"></i>詞彙分級
+            </div>
+            <Link to='/' className='item'>首頁</Link>
+            <a href='#' onClick={this.open書寫規範.bind(this)} className='item'>書寫規範</a>
+            <Link to='/%E5%AD%97%E6%95%B8/' className='item'>字數</Link>
+            <a className='item' href='https://www.facebook.com/groups/1176719339085090/1182176585206032/?notif_t=group_activity&notif_id=1479125586448144'>FB社團</a>
+            <a className='item' href='https://github.com/sih4sing5hong5/su5lui3-hun1kip4'>網站程式碼</a>
+          </div>
+        
         <書寫規範 modalIsOpen={this.state.modalIsOpen}
           closeModal={this.close書寫規範.bind(this)}/>
       </header>
